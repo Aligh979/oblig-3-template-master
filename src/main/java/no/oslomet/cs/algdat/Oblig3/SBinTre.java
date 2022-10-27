e799d878efecaa6a1c8963c19a9e8d7d3a79efeb
Aligh979 <s362111@oslomet.no>
2022-10-27 10:44:45 +0200

    Initial commit

---
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
@@ -0,0 +1,208 @@
+package no.oslomet.cs.algdat.Oblig3;
+
+
+import java.util.*;
+
+public class SBinTre<T> {
+    private static final class Node<T>   // en indre nodeklasse
+    {
+        private T verdi;                   // nodens verdi
+        private Node<T> venstre, høyre;    // venstre og høyre barn
+        private Node<T> forelder;          // forelder
+
+        // konstruktør
+        private Node(T verdi, Node<T> v, Node<T> h, Node<T> forelder) {
+            this.verdi = verdi;
+            venstre = v;
+            høyre = h;
+            this.forelder = forelder;
+        }
+
+        private Node(T verdi, Node<T> forelder)  // konstruktør
+        {
+            this(verdi, null, null, forelder);
+        }
+
+        @Override
+        public String toString() {
+            return "" + verdi;
+        }
+
+    } // class Node
+
+    private Node<T> rot;                            // peker til rotnoden
+    private int antall;                             // antall noder
+    private int endringer;                          // antall endringer
+
+    private final Comparator<? super T> comp;       // komparator
+
+    public SBinTre(Comparator<? super T> c)    // konstruktør
+    {
+        rot = null;
+        antall = 0;
+        comp = c;
+    }
+
+    public boolean inneholder(T verdi) {
+        if (verdi == null) return false;
+
+        Node<T> p = rot;
+
+        while (p != null) {
+            int cmp = comp.compare(verdi, p.verdi);
+            if (cmp < 0) p = p.venstre;
+            else if (cmp > 0) p = p.høyre;
+            else return true;
+        }
+
+        return false;
+    }
+
+    public int antall() {
+        return antall;
+    }
+
+    public String toStringPostOrder() {
+        if (tom()) return "[]";
+
+        StringJoiner s = new StringJoiner(", ", "[", "]");
+
+        Node<T> p = førstePostorden(rot); // går til den første i postorden
+        while (p != null) {
+            s.add(p.verdi.toString());
+            p = nestePostorden(p);
+        }
+
+        return s.toString();
+    }
+
+    public boolean tom() {
+        return antall == 0;
+    }
+
+    public boolean leggInn(T verdi) {
+
+        //throw new UnsupportedOperationException("Ikke kodet ennå!");
+        Objects.requireNonNull(verdi, "Ulovlig med nullverdier!");
+
+        Node<T> p = rot, q = null;               // p starter i roten
+        int cmp = 0;                             // hjelpevariabel
+
+        while (p != null)       // fortsetter til p er ute av treet
+        {
+            q = p;                                 // q er forelder til p
+            cmp = comp.compare(verdi,p.verdi);     // bruker komparatoren
+            p = cmp < 0 ? p.venstre : p.høyre;     // flytter p
+        }
+
+        // p er nå null, dvs. ute av treet, q er den siste vi passerte
+
+        p = new Node<>(verdi,q);                   // oppretter en ny node
+
+        if (q == null) rot = p;                  // p blir rotnode
+        else if (cmp < 0) q.venstre = p;         // venstre barn til q
+        else q.høyre = p;                        // høyre barn til q
+
+        antall++;                                // én verdi mer i treet
+        return true;                             // vellykket innlegging
+
+    }
+
+
+    public boolean fjern(T verdi) {
+        throw new UnsupportedOperationException("Ikke kodet ennå!");
+    }
+
+    public int fjernAlle(T verdi) {
+        throw new UnsupportedOperationException("Ikke kodet ennå!");
+    }
+
+    public int antall(T verdi) {
+        //throw new UnsupportedOperationException("Ikke kodet ennå!");
+        int count=0;
+        if (verdi==null){
+            return 0;
+        }
+        Node <T> p = rot;
+        while (p!=null) {
+            int com = comp.compare(verdi, p.verdi);
+            if (com < 0)
+                p = p.venstre;
+            else {
+                if (com == 0)
+                    count++;
+                p = p.høyre;
+            }
+        }
+        return count;
+    }
+
+    public void nullstill() {
+        throw new UnsupportedOperationException("Ikke kodet ennå!");
+    }
+
+    private static <T> Node<T> førstePostorden(Node<T> p) {
+        //throw new UnsupportedOperationException("Ikke kodet ennå!");
+        if (p==null){
+            throw new NoSuchElementException("Treet er tomt!");
+        }
+        while (true){
+            if (p.venstre != null) p = p.venstre;
+            else if (p.høyre != null) p = p.høyre;
+            else return p;
+        }
+    }
+
+    private static <T> Node<T> nestePostorden(Node<T> p) {
+        //throw new UnsupportedOperationException("Ikke kodet ennå!");
+        //treet er tomt
+        if (p==null){
+            throw new NoSuchElementException("Treet er tomt!");
+        }
+        //treet har en node
+        else if (p.forelder ==null){
+            p= null;
+        }
+        //siste noden i venstre har kun et barn til høyre
+        else if (p==p.forelder.høyre){
+            p=p.forelder;
+        }
+        //noden har et barn til venstre eller to barn
+        else if (p==p.forelder.venstre){
+            //et barn til venstre
+            if (p.forelder.høyre==null){
+                p=p.forelder;
+            }
+            //to barn
+
+            else
+
+                //p= førstePostorden(p.forelder.høyre);
+                p=p.forelder.høyre;
+        }
+        return p;
+    }
+
+    public void postorden(Oppgave<? super T> oppgave) {
+        //throw new UnsupportedOperationException("Ikke kodet ennå!");
+    }
+
+    public void postordenRecursive(Oppgave<? super T> oppgave) {
+
+        //postordenRecursive(rot, oppgave);
+    }
+
+    private void postordenRecursive(Node<T> p, Oppgave<? super T> oppgave) {
+        throw new UnsupportedOperationException("Ikke kodet ennå!");
+    }
+
+    public ArrayList<T> serialize() {
+        throw new UnsupportedOperationException("Ikke kodet ennå!");
+    }
+
+    static <K> SBinTre<K> deserialize(ArrayList<K> data, Comparator<? super K> c) {
+        throw new UnsupportedOperationException("Ikke kodet ennå!");
+    }
+
+
+} // ObligSBinTre
